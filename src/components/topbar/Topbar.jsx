224d1c8419ab9a097b21a1879bcc59baad94a879
Lizzieme19 <lizziekamash@gmail.com>
2023-09-06 16:22:36 +0300

    created navbar menu +sidebar menu

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,31 @@
+import "./topbar.scss";
+import { Person, Mail } from "@material-ui/icons";
+
+export default function Topbar({menuOpen, setMenuOpen}) {
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <Person className="icon" />
+            <span>0746445050</span>
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            <span>lizziekamash@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen)}>
+            <span className="line 1"></span>
+            <span className="line 2"></span>
+            <span className="line 3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
